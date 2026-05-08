3173ba1bf0c34ea5bde131dd63277301399c5609
Shamsheer <designcore.dev@gmail.com>
2026-05-08 23:25:50 +0530

    fix: configure CORS to use environment variable for origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,9 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors( {
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+} ));
 app.use(express.json());
 app.use(limiter);
 
